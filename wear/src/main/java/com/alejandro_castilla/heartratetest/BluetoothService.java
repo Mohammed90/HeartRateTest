de3b194b888a9d1b615c3dc1c7de61b9e1cefea6
Alejandro Castilla <acastillaquesada@gmail.com>
2016-03-30 20:27:22 +0200

    Some changes made and Toast notifications added.

--- wear/src/main/java/com/alejandro_castilla/heartratetest/BluetoothService.java
+++ wear/src/main/java/com/alejandro_castilla/heartratetest/BluetoothService.java
@@ -1,9 +1,10 @@
 package com.alejandro_castilla.heartratetest;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.support.wearable.activity.WearableActivity;
-import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by alejandrocq on 16/03/16.
@@ -14,19 +15,27 @@ public class BluetoothService extends WearableActivity {
     private final int REQUEST_ENABLE_BT = 1;
 
     private BluetoothAdapter mBluetoothAdapter = null;
+    private Context mToastContext = null;
 
-    public BluetoothService() {
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public BluetoothService(Context context) {
+        this.mToastContext = context;
     }
 
     public void enableBluetooth() {
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (mBluetoothAdapter == null) {
-            Log.e("ERROR: ", "This device does not support Bluetooth.");
+            Toast.makeText(mToastContext, "This device doesn't support Bluetooth.",
+                    Toast.LENGTH_LONG).show();
         } else if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(mToastContext, "Bluetooth has been enabled.", Toast.LENGTH_LONG).show();
         } else {
-            Log.i("Result: ", "Bluetooth already enabled");
+            Toast.makeText(mToastContext, "Bluetooth is already enabled.", Toast.LENGTH_LONG)
+                    .show();
+//            Log.i("Status:", "Bluetooth is already enabled.");
         }
 
     }

ee2a222f458a322116b9a6afc516c629725ba9c8
Alejandro Castilla <acastillaquesada@gmail.com>
2016-04-02 22:07:57 +0200

    BluetoothService.java is implemented now as a real Android Service.

--- wear/src/main/java/com/alejandro_castilla/heartratetest/BluetoothService.java
+++ wear/src/main/java/com/alejandro_castilla/heartratetest/BluetoothService.java
@@ -1,14 +1,16 @@
 package com.alejandro_castilla.heartratetest;
 
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.wearable.activity.WearableActivity;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by alejandrocq on 16/03/16.
@@ -16,39 +18,41 @@ import android.widget.Toast;
 
 enum BluetoothStatus { IDLE, DISCOVERING, NOT_SUPPORTED }
 
-public class BluetoothService extends WearableActivity {
+public class BluetoothService extends Service {
 
-    //Request code to enable Bluetooth.
-    private final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "BluetoothService";
+    public static BluetoothStatus mBluetoothStatus;
+
+    private final IBinder mBluetoothServiceBinder = new BluetoothServiceBinder();
     private BluetoothAdapter mBluetoothAdapter = null;
     private BroadcastReceiver mReceiver;
-    private Context mMainContext = null;
-    private BluetoothStatus mBluetoothStatus;
 
-    public BluetoothService(Context context) {
-        this.mMainContext = context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 
-    public void enableBluetooth() {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "BluetoothService has been started.");
+        return super.onStartCommand(intent, flags, startId);
+    }
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(mMainContext, "This device doesn't support Bluetooth.",
-                    Toast.LENGTH_LONG).show();
-            mBluetoothStatus = BluetoothStatus.NOT_SUPPORTED;
-        } else if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
-            Toast.makeText(mMainContext, "Bluetooth has been enabled.", Toast.LENGTH_LONG).show();
-            mBluetoothStatus = BluetoothStatus.IDLE;
-        } else {
-            Toast.makeText(mMainContext, "Bluetooth is already enabled.", Toast.LENGTH_LONG)
-                    .show();
-            mBluetoothStatus = BluetoothStatus.IDLE;
-//            Log.i("Status:", "Bluetooth is already enabled.");
-        }
 
+    // We don't want this service to be binded for the moment, so we return null.
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBluetoothServiceBinder;
+    }
+
+    public void setmBluetoothAdapter(BluetoothAdapter mBluetoothAdapter) {
+        this.mBluetoothAdapter = mBluetoothAdapter;
     }
 
     public void startDiscoveryOfDevices() {
@@ -72,18 +76,25 @@ public class BluetoothService extends WearableActivity {
         };
 
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        mMainContext.registerReceiver(mReceiver, filter);
-        Log.i("Info", "Receiver registered.");
+        this.registerReceiver(mReceiver, filter);
+        Log.i(TAG, "Receiver registered.");
 
     }
 
     public void stopDiscoveryOfDevices() {
-        mMainContext.unregisterReceiver(mReceiver);
+        this.unregisterReceiver(mReceiver);
         mBluetoothAdapter.cancelDiscovery();
-        Log.i("Info", "Receiver unregistered.");
+        Log.i(TAG, "Receiver unregistered.");
     }
 
-    public BluetoothStatus getmBluetoothStatus() {
-        return mBluetoothStatus;
+
+    /* Class used to bind with the client (MainActivity.java) */
+
+    public class BluetoothServiceBinder extends Binder {
+
+        public BluetoothService getService() {
+            return BluetoothService.this;
+        }
     }
+
 }
--- wear/src/main/java/com/alejandro_castilla/heartratetest/MainActivity.java
+++ wear/src/main/java/com/alejandro_castilla/heartratetest/MainActivity.java
@@ -1,19 +1,70 @@
 package com.alejandro_castilla.heartratetest;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
     private ImageButton mBtnStart;
     private ImageButton mBtnPause;
+
+    /* Some declarations in order to manage Bluetooth. */
+    private final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private Intent mBluetoothServiceIntent;
     private BluetoothService mBluetoothService;
-    private BluetoothStatus mBluetoothStatus;
+    private boolean mBound = false;
+
+    /* ServiceConnection declaration to connect to BluetoothService */
+
+    private ServiceConnection mBluetoothServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BluetoothService.BluetoothServiceBinder mBluetoothServiceBinder =
+                    (BluetoothService.BluetoothServiceBinder) service;
+            mBluetoothService = mBluetoothServiceBinder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
+
+
+    public void enableBluetooth() {
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "This device doesn't support Bluetooth.",
+                    Toast.LENGTH_LONG).show();
+            BluetoothService.mBluetoothStatus = BluetoothStatus.NOT_SUPPORTED;
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+        } else {
+            Toast.makeText(this, "Bluetooth is already enabled.", Toast.LENGTH_LONG)
+                    .show();
+            BluetoothService.mBluetoothStatus = BluetoothStatus.IDLE;
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +83,8 @@ public class MainActivity extends WearableActivity {
                     public void onClick(View v) {
                         mBtnStart.setVisibility(ImageButton.GONE);
                         mBtnPause.setVisibility(ImageButton.VISIBLE);
-                        mTextView.setText("Please wait...");
-                        mBluetoothService.enableBluetooth();
-                        if (mBluetoothService.getmBluetoothStatus() == BluetoothStatus.IDLE) {
-                            mBluetoothService.startDiscoveryOfDevices();
-                        }
-
+                        mBluetoothService.setmBluetoothAdapter(mBluetoothAdapter);
+                        mBluetoothService.startDiscoveryOfDevices();
 
                     }
                 });
@@ -46,13 +93,47 @@ public class MainActivity extends WearableActivity {
         });
 
         setAmbientEnabled();
-        mBluetoothService = new BluetoothService(this);
 
     }
 
+
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onStart() {
+        super.onStart();
+        enableBluetooth();
+        // Finish the application if Bluetooth is not supported.
+        if (BluetoothService.mBluetoothStatus == BluetoothStatus.NOT_SUPPORTED) {
+            finish();
+        } else {
+            // Start BluetoothService
+            mBluetoothServiceIntent = new Intent(this, BluetoothService.class);
+            bindService(mBluetoothServiceIntent, mBluetoothServiceConnection,
+                    Context.BIND_AUTO_CREATE);
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
         mBluetoothService.stopDiscoveryOfDevices();
+        unbindService(mBluetoothServiceConnection);
+        mBound=false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(this, "Bluetooth has been enabled.", Toast.LENGTH_LONG).show();
+                    BluetoothService.mBluetoothStatus = BluetoothStatus.IDLE;
+                } else {
+                    Toast.makeText(this, "Error while activating Bluetooth.",
+                            Toast.LENGTH_LONG).show();
+                    finish();
+                    return;
+                }
+        }
     }
 }
